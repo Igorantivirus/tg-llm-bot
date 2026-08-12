275bce50e532d07a1dc23a2ac7752d65eb6ae1d6
Igorantivirus <igorantivirus@yandex.ru>
2026-08-12 22:44:26 +0700

    Добавлена сериализация для новых структур

--- src/config/ConfigJsonSerialization.hpp
+++ src/config/ConfigJsonSerialization.hpp
@@ -2,9 +2,13 @@
 
 #include <nlohmann/json.hpp>
 
+#include "AccessRightsConfig.hpp"
 #include "AppConfig.hpp"
+#include "UrlConfig.hpp"
 
 namespace config
 {
-NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE_WITH_DEFAULT(AppConfig, token);
-}
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE_WITH_DEFAULT(UrlConfig, host, port);
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE_WITH_DEFAULT(AccessRights, creator, admins, personalChats, groups);
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE_WITH_DEFAULT(AppConfig, token, defaultModel, openAiUrl, adminsSettings);
+} // namespace config
