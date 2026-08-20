f80888451aca0b1e7323c1488edc42457d05b001
Igorantivirus <igorantivirus@yandex.ru>
2026-08-20 23:44:43 +0700

    Отправитель сообщений в телеграмм

--- src/bot/BotMessageSener.hpp
+++ src/bot/BotMessageSener.hpp
@@ -1,9 +1,12 @@
 #pragma once
 
-#include <openaiprocessor/MessageSender.hpp>
+#include <openai/MessageSender.hpp>
 #include <tgbot/Api.h>
 
-class BotMessageSener : public MessageSender
+namespace bot
+{
+
+class BotMessageSener : public openai::MessageSender
 {
 public:
     BotMessageSener(const TgBot::Api &api)
@@ -11,20 +14,31 @@ public:
     {
     }
 
-    void startSendMessages(const ChatID chatId) override
+    MessageID sendMessage(const ChatID chatId, std::string message) override
     {
+        if(message.empty())
+            return 0;
+        auto msgPtr = api_.sendMessage(chatId, message);
+        if (msgPtr)
+            return msgPtr->messageId;
+        return 0;
     }
-    void stopSendMessages(const ChatID chatId) override
+    void replaceMessage(const ChatID chatId, const MessageID msgId, std::string message) override
     {
+        api_.editMessageText(message, chatId, msgId);
     }
-    void sendNextMessage(const ChatID chatId, const std::string &msg) override
+    void sendError(const ChatID chatId, utils::ErrorCode err) override
     {
+        api_.sendMessage(chatId, err.message());
     }
-    void sendMessage(const ChatID chatId, const std::string &msg) override
+    void replaceMessageWithError(const ChatID chatId, const MessageID msgId, std::string message, utils::ErrorCode err) override
     {
-        api_.sendMessage(chatId, msg);
+        message += "\nError: " + err.message();
+        replaceMessage(chatId, msgId, std::move(message));
     }
 
 private:
     const TgBot::Api &api_;
 };
+
+} // namespace bot
