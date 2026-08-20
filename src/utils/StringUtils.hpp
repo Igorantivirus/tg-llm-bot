7e50a64322219ddd428b9bb3da1e31da87425d93
Igorantivirus <igorantivirus@yandex.ru>
2026-08-20 23:43:12 +0700

    Строковая утилита split

---
+++ src/utils/StringUtils.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace utils
+{
+
+class StringUtils
+{
+public:
+    static std::vector<std::string_view> splitArgs(const std::string &str)
+    {
+        std::vector<std::string_view> res;
+
+        std::size_t begin = 0;
+        for (std::size_t end = 0; end < str.size(); ++end)
+        {
+            bool begSpace = isSpaceSymbol(str[begin]);
+            bool endSpace = isSpaceSymbol(str[end]);
+            if (endSpace && !begSpace) // Нашли
+            {
+                res.push_back(std::string_view(str.begin() + begin, str.begin() + end));
+                begin = end;
+            }
+            else if (begSpace && !endSpace)
+                begin = end;
+        }
+        if (!isSpaceSymbol(str[begin]))
+            res.push_back(std::string_view(str.begin() + begin, str.end()));
+        return res;
+    }
+
+private:
+    constexpr static bool isSpaceSymbol(const char c)
+    {
+        return c == ' ' || c == '\t' || c == '\n';
+    }
+};
+
+} // namespace utils
