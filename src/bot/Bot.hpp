b303adda97b2a979adef00d65bd55de863aebc26
Igorantivirus <igorantivirus@yandex.ru>
2026-08-20 23:45:41 +0700

    Базовый функционал

--- src/bot/Bot.hpp
+++ src/bot/Bot.hpp
@@ -5,20 +5,25 @@
 #include <tgbot/Bot.h>
 
 #include <config/AppConfig.hpp>
-#include <openaiprocessor/MessageProcessor.hpp>
+#include <openai/ChatsProcessor.hpp>
 #include <tgbot/TgLongPoll.h>
 #include <utils/MethodBinder.hpp>
+#include <utils/StringUtils.hpp>
 
 #include "BotMessageSener.hpp"
+
+namespace bot
+{
 
 class Bot
 {
 public:
-    Bot(config::AppConfig config)
-        : bot_(std::move(config.token)),
+    Bot(std::string tgBotApiToken, openai::ChatsProcessor &processor)
+        : bot_(std::move(tgBotApiToken)),
           sender_(bot_.getApi()),
-          processor_(sender_)
+          processor_(processor)
     {
+        processor_.setSender(sender_);
         initHendlers();
     }
 
@@ -29,20 +34,58 @@ public:
     }
 
 private:
-    TgBot::Bot bot_;
-    BotMessageSener sender_;
-    MessageProcessor processor_;
+    TgBot::Bot              bot_;
+    BotMessageSener         sender_;
+    openai::ChatsProcessor &processor_;
 
 private:
     void initHendlers()
     {
+        bot_.getEvents().onCommand("start", utils::buildMethod(&Bot::onStart, this));
+        bot_.getEvents().onCommand("models", utils::buildMethod(&Bot::onModels, this));
+        bot_.getEvents().onCommand("model", utils::buildMethod(&Bot::onModel, this));
+        bot_.getEvents().onCommand("currentmodel", utils::buildMethod(&Bot::onCurrentModel, this));
         bot_.getEvents().onNonCommandMessage(utils::buildMethod(&Bot::onMessage, this));
     }
 
 private:
+    void onStart(std::shared_ptr<TgBot::Message> msg)
+    {
+        sender_.sendMessage(msg->chat->id, "Прив");
+    }
+
+    void onModels(std::shared_ptr<TgBot::Message> msg)
+    {
+        auto        models = processor_.getModels();
+        std::string resMsg;
+        for (auto &model : models)
+        {
+            resMsg += std::move(model);
+            resMsg.push_back('\n');
+        }
+        sender_.sendMessage(msg->chat->id, std::move(resMsg));
+    }
+
+    void onModel(std::shared_ptr<TgBot::Message> msg)
+    {
+        auto args = utils::StringUtils::splitArgs(msg->text.value());
+        if (args.size() != 2)
+            sender_.sendMessage(msg->chat->id, "Error\n");
+
+        processor_.setModelToChat(msg->chat->id, std::string(args[1]));
+    }
+
+    void onCurrentModel(std::shared_ptr<TgBot::Message> msg)
+    {
+        std::string model = processor_.getSettings(msg->chat->id).model;
+        sender_.sendMessage(msg->chat->id, "Current model: " + model);
+    }
+
     void onMessage(std::shared_ptr<TgBot::Message> msg)
     {
         if (msg->text)
-            processor_.processMessage(msg->chat->id, msg->text.value());
+            processor_.sendMessage(msg->chat->id, msg->text.value());
     }
 };
+
+} // namespace bot
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
-#include "dto/Role.hpp"
+#include "bot/Bot.hpp"
+#include "config/AppConfig.hpp"
+#include "config/ConfigReader.hpp"
 #include "openai/ChatsProcessor.hpp"
-#include "openai/MessageSender.hpp"
 #include <boost/asio.hpp>
 #include <boost/asio/any_io_executor.hpp>
 #include <boost/asio/awaitable.hpp>
@@ -12,137 +13,19 @@
 #include <boost/beast/http/impl/read.hpp>
 #include <boost/beast/http/parser_fwd.hpp>
 #include <boost/beast/http/string_body_fwd.hpp>
+#include <config/ConfigJsonSerialization.hpp>
+#include <cstdlib>
 #include <dto/ChatCompletionsRequest.hpp>
+#include <exception>
 #include <expected>
 #include <iostream>
 #include <magic_enum/magic_enum.hpp>
 #include <net/Types.hpp>
 #include <openai/Api.hpp>
+#include <stop_token>
 #include <string>
+#include <thread>
 #include <utils/MethodBinder.hpp>
-
-// void initRequestFields(net::BeastRequest &req, const std::string fullHost)
-// {
-//     req.set(http::field::host, fullHost);
-//     req.set(http::field::content_type, "application/json");
-//     req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
-//     req.prepare_payload();
-// }
-
-// asio::awaitable<void> foo(const std::string_view host, const std::string_view port)
-// {
-//     dto::ChatCompletionsRequest dto;
-//     dto.model = "igor-ai";
-//     dto.messages.push_back(dto::Message{.role = "system", .content = "Ответь кратко"});
-//     dto.messages.push_back(dto::Message{.role = "user", .content = "Привет, как дела?"});
-//     dto.stream = true;
-
-//     net::BeastRequest req(http::verb::post, "/v1/chat/completions", 11);
-//     if (std::optional<std::string> sdto = dto::serialize(dto); sdto)
-//     {
-//         std::cout << "resuest: " << *sdto << '\n';
-//         req.body() = *sdto;
-//     }
-//     else
-//         co_return;
-//     std::string fullHost;
-//     fullHost.append(host);
-//     fullHost.push_back(':');
-//     fullHost.append(port);
-
-//     initRequestFields(req, fullHost);
-
-//     HttpStream stream(co_await boost::asio::this_coro::executor);
-
-//     auto res = co_await stream.request(host, port, std::move(req));
-//     if (!res)
-//     {
-//         std::cout << "Error: " << res.error().error << ". Stage: " << magic_enum::enum_name(res.error().stage) << '\n';
-//         co_return;
-//     }
-//     auto value = res.value();
-//     if (value.body)
-//         std::cout << "Onbe elem: " << value.header << '\n'
-//                   << value.body.value() << '\n';
-//     else
-//     {
-//         for (auto next = co_await stream.nextChunk(); next && next.value(); next = co_await stream.nextChunk())
-//         {
-//             std::cout << "Next chunk: " << next.value().value() << '\n';
-//         }
-//     }
-// }
-
-class SonsoleSender : public openai::MessageSender
-{
-public:
-    MessageID sendMessage(const ChatID chatId, std::string message)
-    {
-        std::cout << "sendMessage: " << message << "\n\n";
-        return 0;
-    }
-    void addToMessage(const ChatID chatId, const MessageID msgId, std::string message)
-    {
-        std::cout << "addToMessage: " << message << "\n\n";
-    }
-    void replaceMessage(const ChatID chatId, const MessageID msgId, std::string message)
-    {
-        std::cout << "replaceMessage: " << message << "\n\n";
-    }
-
-    MessageID sendError(const ChatID chatId, Error ec)
-    {
-        std::cout << "sendError: " << toString(ec) << "\n\n";
-        return 0;
-    }
-    void addToError(const ChatID chatId, const MessageID msgId, Error ec)
-    {
-        std::cout << "addToError: " << toString(ec) << "\n\n";
-    }
-    void replaceError(const ChatID chatId, const MessageID msgId, Error ec)
-    {
-        std::cout << "replaceError: " << toString(ec) << "\n\n";
-    }
-};
-
-asio::awaitable<void> test1(const std::string_view host, const std::string_view port)
-{
-    SonsoleSender          sender;
-    openai::ChatsProcessor chats(co_await boost::asio::this_coro::executor, sender, std::string(host), std::string(port));
-
-    if (auto res = co_await chats.initModels(); !res)
-        std::cout << res << '\n';
-
-    chats.setModel(0, "igor-ai");
-
-    if(auto res = co_await chats.sendMessage(0, "привет, как дела?");!res)
-        std::cout << "Error: " << res << '\n';
-
-    co_return;
-}
-
-asio::awaitable<void> foo(const std::string_view host, const std::string_view port)
-{
-    openai::Api api(co_await boost::asio::this_coro::executor, std::string(host), std::string(port));
-
-    dto::ChatCompletionsRequest dto;
-    dto.model = "igor-ai";
-    dto.messages.push_back(dto::Message{.role = dto::Role::system, .content = "Ответь кратко"});
-    dto.messages.push_back(dto::Message{.role = dto::Role::user, .content = "Привет, как дела?"});
-    dto.stream = true;
-
-    auto res = co_await api.chatCompletions(std::move(dto));
-    if (!res)
-    {
-        std::cout << "Error\n";
-        co_return;
-    }
-    auto gen = std::get<1>(res.value());
-    for (auto next = co_await gen.next(); next; next = co_await gen.next())
-    {
-        std::cout << "Next value: " << *dto::serialize(*next) << "\n\n";
-    }
-}
 
 int main(int argc, char **argv)
 {
@@ -150,21 +33,45 @@ int main(int argc, char **argv)
     std::system("chcp 65001 > nul");
 #endif
 
-    // dto::ChatCompletionsRequest dto;
-    // dto.model = "igor-ai";
-    // dto.messages.push_back(dto::Message{.role = "system", .content = "Ответь кратко"});
-    // dto.messages.push_back(dto::Message{.role = "user", .content = "Привет, как дела?"});
-    // dto.stream = true;
+    if (argc != 2)
+        return EXIT_FAILURE;
 
-    // chatCompletions(std::move(dto), "localhost:9292");
+    auto configOpt = config::read<config::AppConfig>(argv[1]);
+    if (!configOpt)
+    {
+        std::cerr << "Error: " << configOpt.error() << '\n';
+        return EXIT_FAILURE;
+    }
+    config::AppConfig cnfg = configOpt.value();
 
     asio::io_context io;
 
-    asio::co_spawn(io, test1("100.104.60.3", "9292"), [](std::exception_ptr ptr)
+    std::jthread ioThread([&io](std::stop_token itoken)
     {
+        while (!itoken.stop_requested())
+        {
+            io.run();
+        }
     });
 
-    io.run();
+    openai::ChatsProcessor chats(io.get_executor(), cnfg.openAiUrl.host, cnfg.openAiUrl.port);
+
+    chats.initModels();
+
+    bot::Bot bt(cnfg.token, chats);
+
+    try
+    {
+        bt.run();
+    }
+    catch (const std::exception &er)
+    {
+        std::cout << "Error: " << er.what() << '\n';
+    }
+    catch (...)
+    {
+        std::cout << "Enlnown error\n";
+    }
 
     return EXIT_SUCCESS;
 }
