2fb90af06f5c70b2851490d94dcefda267f10e78
Игорь Антипов <igorantivirus@yandex.ru>
2026-08-20 09:27:31 +0700

    Мендер методов в лямбду

--- src/utils/MethodBinder.hpp
+++ src/utils/MethodBinder.hpp
@@ -7,33 +7,33 @@ namespace utils
 template <class Class, typename Return, typename... Args>
 constexpr auto buildMethod(Return (Class::*method)(Args...), Class *self) noexcept
 {
-    return [self, method](Args &&...args) noexcept(noexcept((self->*method)(std::forward<Args...>(args...)))) -> Return
+    return [self, method](Args &&...args) noexcept(noexcept((self->*method)(std::forward<Args>(args)...))) -> Return
     {
-        return (self->*method)(std::forward<Args...>(args...));
+        return (self->*method)(std::forward<Args>(args)...);
     };
 }
 template <class Class, typename Return, typename... Args>
 constexpr auto buildMethod(Return (Class::*method)(Args...) const, const Class *self) noexcept
 {
-    return [self, method](Args &&...args) noexcept(noexcept((self->*method)(std::forward<Args...>(args...)))) -> Return
+    return [self, method](Args &&...args) noexcept(noexcept((self->*method)(std::forward<Args>(args)...))) -> Return
     {
-        return (self->*method)(std::forward<Args...>(args...));
+        return (self->*method)(std::forward<Args>(args)...);
     };
 }
 template <class Class, typename Return, typename... Args>
 constexpr auto buildMethod(Return (Class::*method)(Args...), Class &self) noexcept
 {
-    return [self = &self, method](Args &&...args) noexcept(noexcept((self->*method)(std::forward<Args...>(args...)))) -> Return
+    return [self = &self, method](Args &&...args) noexcept(noexcept((self->*method)(std::forward<Args>(args)...))) -> Return
     {
-        return (self->*method)(std::forward<Args...>(args...));
+        return (self->*method)(std::forward<Args>(args)...);
     };
 }
 template <class Class, typename Return, typename... Args>
 constexpr auto buildMethod(Return (Class::*method)(Args...) const, const Class &self) noexcept
 {
-    return [self = &self, method](Args &&...args) noexcept(noexcept((self->*method)(std::forward<Args...>(args...)))) -> Return
+    return [self = &self, method](Args &&...args) noexcept(noexcept((self->*method)(std::forward<Args>(args)...))) -> Return
     {
-        return (self->*method)(std::forward<Args...>(args...));
+        return (self->*method)(std::forward<Args>(args)...);
     };
 }
 
