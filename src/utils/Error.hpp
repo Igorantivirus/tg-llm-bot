7684848ad851226a935b6746308d57ba8f710f23
Igorantivirus <igorantivirus@yandex.ru>
2026-08-28 12:40:41 +0700

    Мозданы ошибки для пакета utils

---
+++ src/utils/Error.hpp
@@ -0,0 +1,60 @@
+#pragma once
+
+#include "ErrorNameHelper.hpp"
+#include <array>
+#include <boost/system/detail/error_category.hpp>
+#include <boost/system/detail/error_code.hpp>
+#include <boost/system/is_error_code_enum.hpp>
+#include <cstdint>
+
+namespace utils
+{
+enum class Error : std::uint8_t
+{
+    TooLongDataSize,
+    EncodeError,
+    Unknown
+};
+
+class ErrorCategoryImpl final : public boost::system::error_category
+{
+public:
+    ErrorCategoryImpl() noexcept
+        : boost::system::error_category(0x6a3f12d7b8e04d40ULL)
+    {
+    }
+
+    const char *name() const noexcept override
+    {
+        return "dto";
+    }
+
+    std::string message(int ev) const override
+    {
+        return utils::getName(names, ev);
+    }
+
+    inline static const boost::system::error_category &getCategory() noexcept
+    {
+        static const ErrorCategoryImpl instance;
+        return instance;
+    }
+
+private:
+    constexpr static const std::array<const char *, static_cast<std::size_t>(Error::Unknown) + 1> names = {
+        "Input too large for EVP_EncodeBlock",
+        "EVP_EncodeBlock failed",
+        "Unknown"};
+};
+
+inline boost::system::error_code make_error_code(const Error e) noexcept
+{
+    return {static_cast<int>(e), ErrorCategoryImpl::getCategory()};
+}
+
+} // namespace utils
+
+template <>
+struct boost::system::is_error_code_enum<utils::Error> : std::true_type
+{
+};
