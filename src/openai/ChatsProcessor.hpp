3e6eb4a4aaf82964a25c200358d55c9239039b77
Igorantivirus <igorantivirus@yandex.ru>
2026-09-02 19:16:43 +0700

    Обработчик чатов переписан под новую структуру проекта

--- src/openai/ChatsProcessor.hpp
+++ src/openai/ChatsProcessor.hpp
@@ -1,21 +1,7 @@
 #pragma once
 
-#include <boost/asio/co_spawn.hpp>
-#include <boost/asio/detached.hpp>
-#include <iterator>
-#include <ranges>
-
-#include <boost/asio/awaitable.hpp>
-
-#include "Api.hpp"
-#include "ChatSettings.hpp"
-#include "MessageSender.hpp"
-#include "SettingsRepository.hpp"
-#include "Types.hpp"
-#include "dto/ChatCompletionsResponse.hpp"
-#include "openai/ChatSettings.hpp"
-#include "openai/Types.hpp"
-#include "utils/Types.hpp"
+#include "AdditionalsToMessage.hpp"
+#include "MessagesGenerator/AssistantMessagesGenerator.hpp"
 
 namespace openai
 {
@@ -23,191 +9,80 @@ namespace openai
 class ChatsProcessor
 {
 public:
-    ChatsProcessor(asio::any_io_executor ex, std::string host, std::string port, std::string token = {})
-        : ex_(ex),
-          api_(ex, std::move(host), std::move(port), std::move(token))
+    ChatsProcessor(asio::any_io_executor ex, std::string host, std::string port, std::string apiToken = {})
+        : api_(ex, std::move(host), std::move(port), apiToken)
     {
     }
 
-#pragma region Get
-
-    const std::unordered_set<std::string> &getModels() const
+    const ChatsSettings &settings() const
     {
-        return repo_.getModels();
+        return setts_;
     }
-    const ChatSettings &getSettings(const ChatID chatId)
+    ChatsSettings &settings()
     {
-        return repo_.getSettings(chatId);
+        return setts_;
     }
 
-#pragma endregion
-
-#pragma region Post
-
-    // Очистить контекст чата
-    void clearContext(const ChatID chatId)
+    void addTool(Tool::Ptr tool)
     {
-        repo_.clearHistory(chatId);
-    }
-    // Установить системный промт
-    void setSystem(const ChatID chatId, std::string system)
-    {
-        repo_.setSystemPromt(chatId, std::move(system));
-    }
-    // Установить модель чата
-    void setModelToChat(const ChatID chatId, std::string model)
-    {
-        repo_.setModelToChat(chatId, std::move(model));
-    }
-    // Отправка и обработка сообщения в чате
-    void sendMessage(const ChatID chatId, std::string msg, const dto::Role role = dto::Role::user)
-    {
-        asio::co_spawn(ex_, sendMessageSync(chatId, std::move(msg), role), asio::detached);
+        setts_.tools_[tool->name()] = std::move(tool);
     }
 
-#pragma endregion
-
-#pragma region Init
-
-    // Установить интерфейс отпарвки сообщения
-    void setSender(MessageSender &sender)
-    {
-        sender_ = &sender;
-    }
-    // Инициализировать модели
-    void initModels()
-    {
-        asio::co_spawn(ex_, initModelsSync(), asio::detached);
-    }
-
-#pragma endregion
-
-#pragma region Async
-
-    // Инициализировать модели
-    utils::AsyncResult<void> initModelsSync()
+    utils::AsyncResult<const std::unordered_set<std::string> *> initModels()
     {
         auto modelsDto = co_await api_.models();
         if (!modelsDto)
             co_return std::unexpected(modelsDto.error());
         dto::ModelsResponse modelsRes = std::move(modelsDto.value());
 
-        std::unordered_set<std::string> &&models = modelsRes.data | std::views::transform([](const dto::Model &model) -> std::string
+        setts_.models_ = modelsRes.data | std::views::transform([](const dto::Model &model) -> std::string
         {
             return model.id;
         }) | std::ranges::to<std::unordered_set<std::string>>();
 
-        repo_.setModels(models);
-        co_return utils::empty;
+        co_return &setts_.models_;
     }
 
-    // Отпарвить сообщение и обработать его асинхронно
-    utils::AsyncResult<void> sendMessageSync(const ChatID chatId, std::string msg, const dto::Role role = dto::Role::user)
+    utils::AsyncResult<AssistantMessagesGenerator> chatCompletions(const ChatIdType chatId, std::string msg, AdditionalsToMessage additionals = {})
     {
-        repo_.addMessage(chatId, msg, role);
+        dto::Message startmsg = constructStartMessage(std::move(msg), std::move(additionals));
+        co_return AssistantMessagesGenerator(api_, chatId, std::move(startmsg), setts_);
+    }
 
-        ChatSettings setts = repo_.getSettings(chatId);
-        if (setts.model.empty())
-        {
-            sender_->sendError(chatId, Error::NoSetModel);
-            co_return utils::empty;
-        }
-        dto::ChatCompletionsRequest req;
-        initChatCompletionsRequestBySettings(req, setts);
+private:
+    Api api_;
 
-        auto response = co_await api_.chatCompletions(std::move(req));
-        if (!response)
-        {
-            sender_->sendError(chatId, response.error());
-            co_return utils::empty;
-        }
-        auto var = std::move(response.value());
-        if (var.index() == 0) // не будет покка styream = true
-            co_return sendMessage(std::get<0>(var), chatId);
+    ChatsSettings setts_;
+
+private:
+    static dto::Message constructStartMessage(std::string msg, AdditionalsToMessage additionals)
+    {
+        dto::Message res;
+        res.role = dto::Role::user;
+        if (additionals.empty())
+            res.content = std::move(msg);
         else
-            co_return co_await streamMessages(std::get<1>(var), chatId);
-    }
-
-#pragma endregion
-
-private:
-    asio::any_io_executor ex_;
-    Api                   api_;
-    SettingsRepository    repo_;
-    MessageSender        *sender_;
-
-private:
-    utils::SyncResult<void> sendMessage(dto::ChatCompletionsResponse &dto, const ChatID chatId)
-    {
-        if (dto.choices.empty())
-            return std::unexpected(Error::EmptyResponse);
-
-        dto::Choice &ch = dto.choices[0];
-        if (!ch.message || !ch.message->role || !ch.message->content) // пустое сообщение // TODO: Когда будут tools - сделать обработку вызовов
-            return std::unexpected(Error::EmptyResponse);
-
-        repo_.addMessage(chatId, *ch.message->content, *ch.message->role);
-        sender_->sendMessage(chatId, std::move(*ch.message->content));
-
-        return utils::empty;
-    }
-    void nextMessageToUser(std::string msg, const ChatID chatId, std::optional<MessageID> &msgIdOpt)
-    {
-        if (!msgIdOpt)
-            msgIdOpt = sender_->sendMessage(chatId, std::move(msg));
-        else
-            sender_->replaceMessage(chatId, *msgIdOpt, std::move(msg));
-    }
-    utils::AsyncResult<void> streamMessages(ApiResponseGenerator &gen, const ChatID chatId)
-    {
-        std::string              messageAccum;
-        std::size_t              lastSizeAccum = 0;
-        const std::size_t        criticalMass = 30;
-        auto                     next = co_await gen.next();
-        std::optional<MessageID> msgIdOpt = std::nullopt;
-        while (next && next.value())
         {
-            auto dto = next.value().value();
-            messageAccum += extractContentFromDelta(dto);
-            if (messageAccum.size() >= lastSizeAccum + criticalMass) // добавилось больше, чем критическая масса
-            {
-                nextMessageToUser(messageAccum, chatId, msgIdOpt);
-                lastSizeAccum = messageAccum.size();
-            }
-            next = co_await gen.next();
+            std::vector<dto::ContentPart> parts;
+            parts.reserve(additionals.sumOfDataParts() + 1);
+            parts.emplace_back(dto::TextPart{.text = std::move(msg)});
+            appendParts(parts, std::move(additionals.imagesB64), &dto::ImagePart::image_url);
+            appendParts(parts, std::move(additionals.audiosB64), &dto::AudioPart::input_audio);
+            appendParts(parts, std::move(additionals.filesB64), &dto::FilePart::file);
         }
-        if (messageAccum.size() > lastSizeAccum)
-            nextMessageToUser(std::move(messageAccum), chatId, msgIdOpt);
-        if (!next)
-            co_return std::unexpected(next.error());
 
-        co_return utils::empty;
+        return res;
     }
 
-private:
-    static std::string extractContentFromDelta(const dto::ChatCompletionsResponse &dto)
+    template <typename Container, typename PartType, typename Member>
+    static void appendParts(std::vector<dto::ContentPart> &parts, Container &&container, Member PartType::*member)
     {
-        if (dto.choices.empty())
-            return "";
-
-        const dto::Choice &ch = dto.choices[0];
-        if (!ch.delta || !ch.delta->content) // пустое сообщение // TODO: Когда будут tools - сделать обработку вызовов
-            return "";
-        return *ch.delta->content;
-    }
-
-    static void initChatCompletionsRequestBySettings(dto::ChatCompletionsRequest &req, ChatSettings &setts)
-    {
-        auto &&pr = setts.mesages | std::views::transform([](std::pair<dto::Role, std::string> &msg) -> dto::Message
+        for (auto &elem : std::forward<Container>(container))
         {
-            return dto::Message{.role = msg.first, .content = std::move(msg.second)};
-        }) | std::ranges::to<decltype(dto::ChatCompletionsRequest::messages)>();
-
-        if (!setts.systemPromt.empty())
-            req.messages.push_back(dto::Message{.role = dto::Role::system, .content = std::move(setts.systemPromt)});
-        req.messages.insert(req.messages.end(), std::make_move_iterator(pr.begin()), std::make_move_iterator(pr.end()));
-        req.model = std::move(setts.model);
-        req.stream = true;
+            PartType part;
+            part.*member = std::move(elem);
+            parts.emplace_back(std::move(part));
+        }
     }
 };
 
