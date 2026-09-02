3fc6a4faa17f0cbc8d1ee34e69baae27bd1d9630
Igorantivirus <igorantivirus@yandex.ru>
2026-09-02 19:05:06 +0700

    Обновлены типы

--- src/utils/Types.hpp
+++ src/utils/Types.hpp
@@ -5,11 +5,13 @@
 #include <boost/asio/awaitable.hpp>
 #include <boost/system/detail/error_code.hpp>
 
+namespace asio = boost::asio;
+
 namespace utils
 {
 using ErrorCode = boost::system::error_code;
 template <typename Type>
-using AsyncResult = boost::asio::awaitable<std::expected<Type, ErrorCode>>;
+using AsyncResult = asio::awaitable<std::expected<Type, ErrorCode>>;
 template <typename Type>
 using SyncResult = std::expected<Type, ErrorCode>;
 constexpr SyncResult<void> empty = SyncResult<void>{};
