e0a715e4da72229e1cd2e6c45fc818fd9ae6b4fa
Igorantivirus <igorantivirus@yandex.ru>
2026-09-05 21:28:25 +0700

    Добавлен текст

--- src/app/config/Locale.hpp
+++ src/app/config/Locale.hpp
@@ -14,11 +14,14 @@ struct CommandErrors
 struct Locale
 {
     std::string   currentModel;
+    std::string   modelHelp;
     std::string   systemPromt;
     std::string   cleared;
     std::string   error;
     std::string   info;
     std::string   thinking;
+    std::string   success;
+    std::string   modelSetted;
     CommandErrors commandErrors;
 };
 } // namespace config
