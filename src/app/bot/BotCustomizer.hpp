4cfb474c8ac66881afc96cdad6ca6930d33ccbef
Igorantivirus <igorantivirus@yandex.ru>
2026-09-05 21:13:13 +0700

    Добавлены остальные команлды прав

--- src/app/bot/BotCustomizer.hpp
+++ src/app/bot/BotCustomizer.hpp
@@ -36,10 +36,17 @@ public:
     {
         reg_.registrateCommand(cmnds.clear.command, &handlers::CommandsProcessor::clearCommand, &PermissionChecker::checkBaseCommand, {0, 0});
         reg_.registrateCommand(cmnds.stop.command, &handlers::CommandsProcessor::stopCommand, &PermissionChecker::checkBaseCommand, {0, 0});
+        reg_.registrateCommand(cmnds.stop_all.command, &handlers::CommandsProcessor::stopAllCommand, &PermissionChecker::checkBaseCommand, {0, 0});
         reg_.registrateCommand(cmnds.system.command, &handlers::CommandsProcessor::systemCommand, &PermissionChecker::checkBaseCommand, {0, 1, true});
-        // reg_.registrateCommand(cmnds.model.command, &handlers::CommandsProcessor::modelCommand, &PermissionChecker::checkBaseCommand, {1, 1});
         reg_.registrateCommand(cmnds.models.command, &handlers::CommandsProcessor::modelsCommand, &PermissionChecker::checkBaseCommand, {0, 0});
-        reg_.registrateCommand(cmnds.make_admin.command, &handlers::CommandsProcessor::makeAdmin, &PermissionChecker::checkBaseCommand, {0, 1});
+        reg_.registrateCommand(cmnds.make_admin.command, &handlers::CommandsProcessor::makeAdmin, &PermissionChecker::checkPermissionCommand, {1, 1});
+        reg_.registrateCommand(cmnds.remove_admin.command, &handlers::CommandsProcessor::removeAdmin, &PermissionChecker::checkPermissionCommand, {1, 1});
+        reg_.registrateCommand(cmnds.ban_user.command, &handlers::CommandsProcessor::banUser, &PermissionChecker::checkPermissionCommand, {1, 1});
+        reg_.registrateCommand(cmnds.unban_user.command, &handlers::CommandsProcessor::unbanUser, &PermissionChecker::checkPermissionCommand, {1, 1});
+        reg_.registrateCommand(cmnds.add_group.command, &handlers::CommandsProcessor::addGroup, &PermissionChecker::checkPermissionCommand, {0, 1});
+        reg_.registrateCommand(cmnds.remove_group.command, &handlers::CommandsProcessor::removeGroup, &PermissionChecker::checkPermissionCommand, {0, 1});
+        reg_.registrateCommand(cmnds.add_chat.command, &handlers::CommandsProcessor::addChat, &PermissionChecker::checkPermissionCommand, {0, 1});
+        reg_.registrateCommand(cmnds.remove_chat.command, &handlers::CommandsProcessor::removeChat, &PermissionChecker::checkPermissionCommand, {0, 1});
 
         reg_.registrateMessageInChat(&handlers::MessagesProcessor::addMessage);
         reg_.registrateMessageAddress(&handlers::MessagesProcessor::processMessage);
--- src/app/handlers/CommandsProcessor.hpp
+++ src/app/handlers/CommandsProcessor.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <charconv>
+#include <optional>
+
 #include <tgbot/tgbot.h>
 
 #include <app/Types.hpp>
@@ -23,31 +26,20 @@ public:
         core::OperationInfo::Ptr info = std::make_shared<core::OperationInfo>(msg->chat->id);
         co_await operator_.clear(info);
     }
-
     boost::asio::awaitable<void> stopCommand(std::vector<std::string>, TgBot::Message::Ptr msg)
     {
         core::OperationInfo::Ptr info = std::make_shared<core::OperationInfo>(msg->chat->id);
         co_await operator_.stop(info);
     }
-
     boost::asio::awaitable<void> stopAllCommand(std::vector<std::string>, TgBot::Message::Ptr msg)
     {
         core::OperationInfo::Ptr info = std::make_shared<core::OperationInfo>(msg->chat->id);
         co_await operator_.stopAll(info);
     }
-
     boost::asio::awaitable<void> modelsCommand(std::vector<std::string>, TgBot::Message::Ptr msg)
     {
         core::OperationInfo::Ptr info = std::make_shared<core::OperationInfo>(msg->chat->id);
         co_await operator_.models(info);
-    }
-
-    boost::asio::awaitable<void> modelCommand(std::vector<std::string> args, TgBot::Message::Ptr msg)
-    {
-        core::OperationInfo::Ptr info = std::make_shared<core::OperationInfo>(msg->chat->id);
-        if (!args.empty())
-            co_await operator_.setModel(info, std::move(args[0]));
-        co_await operator_.model(info);
     }
     boost::asio::awaitable<void> systemCommand(std::vector<std::string> args, TgBot::Message::Ptr msg)
     {
@@ -60,6 +52,42 @@ public:
 
     boost::asio::awaitable<void> makeAdmin(std::vector<std::string> args, TgBot::Message::Ptr msg)
     {
+        co_await applyEdit(std::move(args), std::move(msg), &permissions::Editor::makeAdmin);
+    }
+    boost::asio::awaitable<void> removeAdmin(std::vector<std::string> args, TgBot::Message::Ptr msg)
+    {
+        co_await applyEdit(std::move(args), std::move(msg), &permissions::Editor::makeUnadmin);
+    }
+    boost::asio::awaitable<void> banUser(std::vector<std::string> args, TgBot::Message::Ptr msg)
+    {
+        co_await applyEdit(std::move(args), std::move(msg), &permissions::Editor::ban);
+    }
+    boost::asio::awaitable<void> unbanUser(std::vector<std::string> args, TgBot::Message::Ptr msg)
+    {
+        co_await applyEdit(std::move(args), std::move(msg), &permissions::Editor::unban);
+    }
+    boost::asio::awaitable<void> addGroup(std::vector<std::string> args, TgBot::Message::Ptr msg)
+    {
+        co_await applyEdit(std::move(args), std::move(msg), &permissions::Editor::addGroup, msg->chat->id, false);
+    }
+    boost::asio::awaitable<void> removeGroup(std::vector<std::string> args, TgBot::Message::Ptr msg)
+    {
+        co_await applyEdit(std::move(args), std::move(msg), &permissions::Editor::dellGroup, msg->chat->id, false);
+    }
+    boost::asio::awaitable<void> addChat(std::vector<std::string> args, TgBot::Message::Ptr msg)
+    {
+        co_await applyEdit(std::move(args), std::move(msg), &permissions::Editor::addPersonalChat, msg->chat->id);
+    }
+    boost::asio::awaitable<void> removeChat(std::vector<std::string> args, TgBot::Message::Ptr msg)
+    {
+        co_await applyEdit(std::move(args), std::move(msg), &permissions::Editor::dellPersonalChat, msg->chat->id);
+    }
+
+private:
+    using EditMethod = bool (permissions::Editor::*)(const app::UserId);
+
+    boost::asio::awaitable<void> applyEdit(std::vector<std::string> args, TgBot::Message::Ptr msg, EditMethod method, std::optional<app::UserId> defaultId = std::nullopt, const bool allowReply = true)
+    {
         app::UserId id = {};
         if (!args.empty())
         {
@@ -68,13 +96,24 @@ public:
             if (ec != std::errc())
                 co_return (co_await sender_.sendMessage(msg->messageId, "Неверный user id."));
         }
+        else if (auto replyId = allowReply ? getReplyUserId(msg) : std::optional<app::UserId>{})
+            id = *replyId;
+        else if (defaultId)
+            id = *defaultId;
         else
             co_return (co_await sender_.sendMessage(msg->messageId, "Ошибка."));
 
-        if (!editor_.makeAdmin(id))
+        if (!(editor_.*method)(id))
             co_return (co_await sender_.sendMessage(msg->messageId, "Ошибка"));
         readwriter_.save();
         co_return (co_await sender_.sendMessage(msg->messageId, "Успех."));
+    }
+
+    static std::optional<app::UserId> getReplyUserId(const TgBot::Message::Ptr &msg)
+    {
+        if (!msg->replyToMessage || !msg->replyToMessage->from)
+            return std::nullopt;
+        return msg->replyToMessage->from->id;
     }
 
 private:
