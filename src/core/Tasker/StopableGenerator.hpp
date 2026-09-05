2b3c37f4fa9d36ba01a85fc1dd387c40df8e9274
Igorantivirus <igorantivirus@yandex.ru>
2026-09-05 10:43:49 +0700

    Генератор сделан NoMovable

--- src/core/Tasker/StopableGenerator.hpp
+++ src/core/Tasker/StopableGenerator.hpp
@@ -1,11 +1,12 @@
 #pragma once
 
+#include "utils/NoMovable.hpp"
 #include <openai/MessagesGenerator/AssistantMessagesGenerator.hpp>
 #include <utils/StreamGenerator.hpp>
 
 namespace core
 {
-class StopableGenerator : public utils::StreamGenerator<std::string>
+class StopableGenerator : public utils::StreamGenerator<std::string>, public utils::NoMovable
 {
 public:
     StopableGenerator(openai::AssistantMessagesGenerator gen, const bool &stop)
